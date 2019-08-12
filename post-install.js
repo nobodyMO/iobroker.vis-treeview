0961b314b72c87ec79df67ccd02fcd2223e938d6
Markus Korporal <markus@korporal.de>
2019-08-12 13:51:42 +0200

    Update post-install.js

--- post-install.js
+++ post-install.js
@@ -10,7 +10,16 @@
 
 'use strict'
 
-var gentlyCopy = require('gently-copy')
-gentlyCopy('node_modules/jquery.fancytree/dist', 'widgets/treeview/js/fancytree');
-
-gentlyCopy('../jquery.fancytree/dist', 'widgets/treeview/js/fancytree');
+var gentlyCopy = require('gently-copy');
+var fs = require('fs');
+if (!fs.existsSync('widgets/treeview/js')){
+    fs.mkdirSync('widgets/treeview/js');
+}
+if (!fs.existsSync('widgets/treeview/js/fancytree')){
+    fs.mkdirSync('widgets/treeview/js/fancytree');
+}
+if (!fs.existsSync('node_modules/jquery.fancytree/dist')){
+  gentlyCopy('node_modules/jquery.fancytree/dist', 'widgets/treeview/js/fancytree');
+} else {
+  gentlyCopy('../jquery.fancytree/dist', 'widgets/treeview/js/fancytree');
+}
