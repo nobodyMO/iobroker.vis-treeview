c0bb29e3604fb500962c57f46e26b45d6693a840
Markus Korporal <markus@korporal.de>
2019-08-12 14:08:26 +0200

    Update post-install.js

--- post-install.js
+++ post-install.js
@@ -18,8 +18,10 @@ if (!fs.existsSync('widgets/treeview/js')){
 if (!fs.existsSync('widgets/treeview/js/fancytree')){
     fs.mkdirSync('widgets/treeview/js/fancytree');
 }
-if (fs.existsSync('node_modules/jquery.fancytree/dist')){
-  gentlyCopy('node_modules/jquery.fancytree/dist', 'widgets/treeview/js/fancytree');
-} else {
-  gentlyCopy('../jquery.fancytree/dist', 'widgets/treeview/js/fancytree');
-}
+
+ncp((fs.existsSync('node_modules/jquery.fancytree/dist')?'node_modules/jquery.fancytree/dist':'../jquery.fancytree/dist')'widgets/treeview/js/fancytree', function (err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Copied fancytree files!'
+);
