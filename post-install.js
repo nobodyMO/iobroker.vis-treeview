83c93496a81373b5499947922319a6fe17f73fbc
Markus Korporal <markus@korporal.de>
2019-08-12 12:50:35 +0200

    Update post-install.js

--- post-install.js
+++ post-install.js
@@ -1,1 +1,14 @@
 
+
+// post-install.js
+
+/**
+ * Script to run after npm install
+ *
+ * Copy selected files from package in Widget directory
+ */
+
+'use strict'
+
+var gentlyCopy = require('gently-copy')
+gentlyCopy('node_modules/jquery.fancytree/dist', 'widgets/treeview/js/fancytree');
